777e180831b800b62a82f85ed5db38ef350dc1d8
lukeleech <lukeyoobee@gmail.com>
2022-09-29 11:58:25 +1300

    User functionality added, Contactt model created but not yet implemented. No static or Html yet

--- kiwipages/base/models.py
+++ kiwipages/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    phnum = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    PCM = models.CharField(max_length=10)
+    ProfilePicture = models.ImageField(upload_to='')
+    Description = models.CharField(max_length=150)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+      ordering = ['name']
--- kiwipages/base/views.py
+++ kiwipages/base/views.py
@@ -1,5 +1,57 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 
+# login view checks entered username and password against database and
+# either logs you in or return error if something went wrong
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
 
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+# django logout function
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def registerPage(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+    return render(request, 'base/login_register.html', {'form': form})
+
+def home(request):
+
+    return render(request, 'base/home.html')
