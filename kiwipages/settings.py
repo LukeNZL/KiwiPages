5c48e6c1e57c8a1a50ab0d09e55d6d70a6330591
LukeNZL <lukeyoobee@gmail.com>
2022-11-17 10:38:17 +1300

    Added hard-coded static file values in settings.py

--- kiwipages/settings.py
+++ kiwipages/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'storages', #for static file deployment
+
     'kiwipages',
 ]
 
@@ -146,8 +148,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+AWS_STORAGE_BUCKET_NAME = 'kiwipages-app-bucket'
+AWS_S3_REGION_NAME = 'ap-southeast-2'
+
+
+
+AWS_S3_ACCESS_KEY_ID = 'AKIA4TMQ4WVOEOOYV4MQ'
+AWS_S3_SECRET_ACCESS_KEY = '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'
+
+
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+   'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+#AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
+AWS_LOCATION = 'static'
+STATICFILES_DIRS = [
+   'static',
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATIC_ROOT = 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
